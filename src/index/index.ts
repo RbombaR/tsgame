aecbf30ffcd1c38c17fae7ca3947ba5018af2fd3
Juraj Mäsiar <juraj.masiar@gmail.com>
2019-11-26 16:04:37 +0100

    pridali sme nejaky css a daky kod

--- src/index/index.ts
+++ src/index/index.ts
@@ -5,15 +5,46 @@ import {buildElement} from '../utils/html';
 async function main() {
   console.log('hello sd game!');
 
-  const buttonNode = buildElement('button', {
-    textContent: 'Click me!',
-    handlers: {
-      onclick: () => buttonNode.textContent = 'Clicked!',
-      onmouseover: () => buttonNode.style.width = '200px',
-    },
-  });
+  const buttonNode = buildElement('div', {
+        style: 'border: 1px solid red',
+        class: 'block',
+        textContent: 'Click me!',
+        handlers: {
+            onclick: () => buttonNode.textContent = 'Clicked!',
+            onmouseover: () => buttonNode.style.width = '200px',
+        },
+    });
+
+
+    const buttonNode2 = buildElement('div', {
+        style: 'border: 1px solid red',
+        class: 'block',
+        textContent: 'Click me!',
+        handlers: {
+            onclick: () => buttonNode.textContent = 'Clicked!',
+            onmouseover: () => buttonNode.style.width = '200px',
+        },
+    });
+    
+
+    buttonNode.appendChild(buttonNode2);
 
   document.body.appendChild(buttonNode);
+  buttonNode.remove();
+  document.body.appendChild(buttonNode);
+
+
+  window.addEventListener('keypress', function (e) {
+      if (e.key === 'a') {
+
+      }
+      switch (e.key) {
+          case 'a':
+              break;
+          case 'b':
+              break;
+      }
+  })
 
 }
 
