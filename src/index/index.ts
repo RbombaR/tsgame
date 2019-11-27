16b89764c165f20e01c4983efe9353792e967ba6
Juraj Mäsiar <juraj.masiar@gmail.com>
2019-11-27 09:42:28 +0100

    - better example

--- src/index/index.ts
+++ src/index/index.ts
@@ -5,49 +5,57 @@ import {buildElement} from '../utils/html';
 async function main() {
   console.log('hello sd game!');
 
-  const buttonNode = buildElement('div', {
-        style: 'border: 1px solid red',
-        class: 'block',
-        textContent: 'Click me!',
-        handlers: {
-            onclick: () => buttonNode.textContent = 'Clicked!',
-            onmouseover: () => buttonNode.style.width = '200px',
-        },
-    });
+  const container = buildElement('div', {
+    style: 'border: 1px solid red',
+    class: 'block big',
+    textContent: 'Click me!',
+    handlers: {
+      onclick: () => container.textContent = 'Clicked!',
+    },
+  });
 
 
-    const buttonNode2 = buildElement('div', {
-        style: 'border: 1px solid red',
-        class: 'block',
-        textContent: 'Click me!',
-        handlers: {
-            onclick: () => buttonNode.textContent = 'Clicked!',
-            onmouseover: () => buttonNode.style.width = '200px',
-        },
-    });
-    
+  const nestedContainer = buildElement('div', {
+    style: 'border: 1px solid red',
+    class: 'block',
+    textContent: 'Click me!',
+    handlers: {
+      onclick: () => container.textContent = 'Clicked!',
+      onmouseover: () => {
+        container.style.width = '200px';
+        container.style.height = '200px';
+      },
+    },
+  });
 
-    buttonNode.appendChild(buttonNode2);
 
-  document.body.appendChild(buttonNode);
-  buttonNode.remove();
-  document.body.appendChild(buttonNode);
+  container.appendChild(nestedContainer);
+
+  document.body.appendChild(container);
+  container.remove();
+  document.body.appendChild(container);
 
 
   window.addEventListener('keypress', function (e) {
-      if (e.key === 'a') {
+    // you can use "if" blocks to distinguish between pressed keys:
+    if (e.key === 'a') {
 
-      }
-      switch (e.key) {
-          case 'a':
-              break;
-          case 'b':
-              break;
-      }
+    }
+    else if(e.key === 'b') {
+
+    }
+    // you can use "switch" to distinguish between pressed keys:
+    switch (e.key) {
+      case 'a':
+        console.log(`user pressed A key`);
+        break;
+      case 'b':
+        console.log(`user pressed B key`);
+        break;
+    }
   })
 
 }
-
 
 
 main();
