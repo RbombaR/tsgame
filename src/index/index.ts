7cdc0bc563475dc23d65b498344bd803cfff0194
Juraj Mäsiar <juraj.masiar@gmail.com>
2019-12-03 15:33:29 +0100

    added play area drawing

--- src/index/index.ts
+++ src/index/index.ts
@@ -1,9 +1,27 @@
 import "../utils/utils.css";
 import "./index.css";
 import {buildElement} from '../utils/html';
+import {timeoutPromise} from "../utils/utils_module";
+import {playArea} from "./play_area";
 
 async function main() {
   console.log('hello sd game!');
+
+
+  console.table(playArea);
+
+  playArea.forEach(row => {
+    row.forEach(item => {
+      const box = buildElement('div', {class: 'box'});
+      document.body.appendChild(box);
+    });
+    const br = buildElement('br');
+    document.body.appendChild(br);
+  });
+
+
+
+
 
   const container = buildElement('div', {
     style: 'border: 1px solid red',
@@ -29,11 +47,11 @@ async function main() {
   });
 
 
-  container.appendChild(nestedContainer);
-
-  document.body.appendChild(container);
-  container.remove();
-  document.body.appendChild(container);
+  // container.appendChild(nestedContainer);
+  //
+  // document.body.appendChild(container);
+  // container.remove();
+  // document.body.appendChild(container);
 
 
   window.addEventListener('keypress', function (e) {
---
+++ src/index/play_area.ts
@@ -0,0 +1,5 @@
+const rows = 5;
+const columns = 4;
+
+export const playArea = Array(rows).fill(0).map(() => Array(columns).fill(0).map(() => ({})));
+
